0db7b0778bcbf35fa56ac5aa499a1e4abb308ec4
Nikolai Leonov <strongardo@gmail.com>
2020-01-31 14:20:42 +0600

    Добавляет вывод статистики

---
+++ js/stat.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var CLOUD_GAP = 10;
+var GIST_HEIGHT = 150;
+var GIST_GAP = 85;
+var BAR_WIDTH = 40;
+var BAR_GAP = 50;
+var NAMES_GAP = 17;
+var TIMES_GAP = 5;
+
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+var getMaxElement = function (arr) {
+  if (arr.length) {
+    var maxElement = arr[0];
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] > maxElement) {
+        maxElement = arr[i];
+      }
+    }
+  } else {
+    maxElement = null;
+  }
+  return maxElement;
+};
+
+window.renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.fillText('Ура вы победили!', 120, 40);
+  ctx.fillText('Список результатов:', 120, 60);
+
+  for (var i = 0; i < names.length; i++) {
+    ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_WIDTH + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + GIST_GAP + GIST_HEIGHT - (GIST_HEIGHT * times[i] / getMaxElement(times)) - TIMES_GAP);
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'hsl(233,' + Math.floor(Math.random() * 100) + '%, 50%)';
+    }
+    ctx.fillRect(CLOUD_X + BAR_WIDTH + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + GIST_GAP + GIST_HEIGHT, BAR_WIDTH, -1 * GIST_HEIGHT * times[i] / getMaxElement(times));
+    ctx.fillStyle = '#000';
+    ctx.fillText(names[i], CLOUD_X + BAR_WIDTH + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + GIST_GAP + GIST_HEIGHT + NAMES_GAP);
+  }
+};
